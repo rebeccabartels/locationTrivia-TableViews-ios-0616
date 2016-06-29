3ff63974a04e09a5f5fe7e1c35762652b78c2bde
rebeccabartels <rebeccakbartels@icloud.com>
2016-06-29 09:22:00 -0400

    hi

---
+++ locationTrivia-tableviews/FISTriviaTableViewController.h
@@ -0,0 +1,18 @@
+//
+//  FISTriviaTableViewController.h
+//  locationTrivia-tableviews
+//
+//  Created by Rebecca Bartels on 6/28/16.
+//  Copyright © 2016 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISTriviaTableViewController : UITableViewController
+@property (strong, nonatomic) NSArray *trivia;
+
+
+
+
+
+@end
